b2e49c91a08c987d904d64f80e7c9ecfa859916d
Shashank Cuppala <shashankcuppala@Shashanks-MacBook-Pro.local>
2025-05-05 14:15:22 -0700

    Initial commit: FPGA Card Flipping Memory Game

---
+++ push_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /Users/shashankcuppala/Desktop/FLIP/card-flip-memory-game
+git init
+git add .
+git commit -m "Initial commit: FPGA Card Flipping Memory Game"
+git remote add origin https://github.com/Shanky-2521/card-flip-memory-game.git
+git branch -M main
+git push -u origin main
